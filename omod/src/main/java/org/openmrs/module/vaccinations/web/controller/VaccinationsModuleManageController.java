ed86547cafe69b85058404aa26ba4213a5a84c9c
Serghei L <serghei.luchianov@icchange.ca>
2015-06-16 17:07:16 -0600

    Implemented tabs

--- omod/src/main/java/org/openmrs/module/vaccinations/web/controller/VaccinationsModuleManageController.java
+++ omod/src/main/java/org/openmrs/module/vaccinations/web/controller/VaccinationsModuleManageController.java
@@ -48,8 +48,13 @@ public class  VaccinationsModuleManageController {
         model.addAttribute("user", Context.getAuthenticatedUser());
     }
 
-    @RequestMapping(value = "/module/vaccinations/vaccinationsPortlet", method = RequestMethod.GET)
-    public void PatientBillHistoryController(ModelMap model) {
+    @RequestMapping(value = "/module/vaccinations/portlets/vaccinationsPortlet", method = RequestMethod.GET)
+    public void vaccinationsPortletController(ModelMap model) {
+        model.addAttribute("user", Context.getAuthenticatedUser());
+    }
+
+    @RequestMapping(value = "/module/vaccinations/vaccinationsPage", method = RequestMethod.GET)
+    public void vaccinationsPageController(ModelMap model) {
         model.addAttribute("user", Context.getAuthenticatedUser());
     }
 }
--- omod/src/main/webapp/template/localHeader.jsp
+++ omod/src/main/webapp/template/localHeader.jsp
@@ -12,4 +12,5 @@
 	</li>
 
 	<!-- Add further links here -->
+
 </ul>
