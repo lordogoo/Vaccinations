aff25a6df625d4dd503f8414c83dea64293065ba
Serghei L <serghei.luchianov@icchange.ca>
2015-08-26 15:49:43 -0600

    Fixed unadminister flag

--- omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/VaccinationsResourceController.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/VaccinationsResourceController.java
@@ -139,6 +139,7 @@ public class VaccinationsResourceController {// extends MainResourceController {
 		try
 		{
 			simpleVaccination.setPatient_id(patientId);
+            simpleVaccination.setAdministered(!simpleVaccination.getUnadminister());
 
 			//If an object already exists in the session, save using that object
 			Vaccination oldVaccination = Context.getService(VaccinationsService.class).getVaccinationByVaccinationId(vaccinationId);
