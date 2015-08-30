a4b55c4d379a416f967b67b287d8b5a51b62d7d9
Jon Cairo <joncairo@gmail.com>
2015-08-30 15:47:00 -0600

    Button labelling, reset adverse form on close.

--- frontend/src/app/vaccination/administered/administered_vaccination.controller.js
+++ frontend/src/app/vaccination/administered/administered_vaccination.controller.js
@@ -32,6 +32,7 @@ angular.module('vaccinations')
     };
 
     $scope.toggleReactionForm = function() {
+        $scope.resetFormDataToDefaults();
         $scope.state.editFormOpen = false;
         $scope.state.adverseFormOpen = !$scope.state.adverseFormOpen;
     };
