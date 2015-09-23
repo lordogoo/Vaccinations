9667ffbc961454024373bd88d1dcee0661ac544f
Serghei L <serghei.luchianov@icchange.ca>
2015-09-23 12:16:12 -0600

    Retroactive data entry + Unadministering bugfix
    Final touches on retroactive data entry and Unadministering vaccinations

--- omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/VaccinationsResourceController.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/VaccinationsResourceController.java
@@ -138,18 +138,21 @@ public class VaccinationsResourceController {// extends MainResourceController {
 			@RequestBody SimpleVaccination simpleVaccination, @PathVariable int vaccinationId, @PathVariable int patientId) throws Exception {
 		try
 		{
-			simpleVaccination.setPatient_id(patientId);
+			//simpleVaccination.setPatient_id(patientId);
             Vaccination newVaccination = new Vaccination(simpleVaccination);
 
             if (simpleVaccination.getUnadminister()) {
                 //Tagging vaccination as not administered
                 newVaccination.setAdministered(false);
+				//simpleVaccination changes are strictly for the purpose of the audit log record, hence only recording unadministering
                 simpleVaccination.setAdministered(false);
 
                 //If a vaccination is a scheduled one, then revert to the template, otherwise do nothing
                 if (simpleVaccination.getScheduled() == true && simpleVaccination.getSimpleVaccine().getNumeric_indication() != null) {
                     newVaccination.updateFromOwnVaccine();
-                    simpleVaccination.updateFromOwnSimpleVaccine();
+					newVaccination.setManufacturer(null);
+					newVaccination.setLot_number(null);
+					newVaccination.setScheduled(true);
                 }
             }
 
--- omod/src/main/webapp/vaccinationsPage.jsp
+++ omod/src/main/webapp/vaccinationsPage.jsp
@@ -5,10 +5,10 @@
 
 <c:choose>
 	<c:when test="${retroactive != 'true'}">
-		<a href="../vaccinations/vaccinationsPage.form?patientId=${patientId}&retroactive=true">Retroactive data entry</a>
+		<a href="../vaccinations/vaccinationsPage.form?patientId=${patientId}&retroactive=true">Enable retroactive data entry</a>
 	</c:when>
 	<c:otherwise>
-		<a href="../vaccinations/vaccinationsPage.form?patientId=${patientId}&retroactive=false">Non-retroactive data entry</a>
+		<a href="../vaccinations/vaccinationsPage.form?patientId=${patientId}&retroactive=false">Disable retroactive data entry</a>
 	</c:otherwise>
 </c:choose>
 <openmrs:portlet id="Vaccinations" moduleId="vaccinations" url="vaccinationsPortlet" />
