f83a9eaf9f1cd59c475cf8c21a185af24a407131
Jon Cairo <joncairo@gmail.com>
2015-09-23 08:29:01 -0600

    Added compiled files from previous commit.

--- omod/src/main/webapp/resources/scripts/app-bd04553e.js
+++ omod/src/main/webapp/resources/scripts/app-bd04553e.js
@@ -17,75 +17,13 @@
     'angular.filter'
 ])
 // Top level angular configs
-.config(["$httpProvider", "$locationProvider", function ($httpProvider, $locationProvider) {
+.config(function ($httpProvider, $locationProvider) {
     $httpProvider.defaults.withCredentials = true;
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
-}]);
-'use strict';
-
-angular.module('vaccinations')
-.controller('StagedVaccinationController', ['$scope', '$filter', 'vaccinationsManager',
-    function ($scope, $filter, vaccinationsManager) {
-    $scope.state = {};
-    $scope.state.administerFormOpen = true;
-
-    $scope.removeStagedVaccination = function () {
-        vaccinationsManager.removeStagedVaccination();
-    };
-
-    $scope.resetFormDataToDefaults = function () {
-        var vaccination = angular.copy($scope.getVaccination());
-        if (vaccination.custom) {
-            vaccination.name = '';
-        }
-        vaccination.administration_date = new Date();
-        vaccination.scheduled_date = new Date();
-        $scope.enteredAdminFormData = vaccination;
-    };
-
-    // Transform a vaccine object into a vaccination object that
-    // can be submitted to the vaccinations endpoint.
-    $scope.transformVaccineToVaccination = function (vaccine) {
-        var vaccination = angular.copy(vaccine);
-        // Create nested objects
-        debugger;
-        vaccination.adverse_reaction_observed = false;
-        vaccination.simpleVaccine = {};
-        vaccination.simpleVaccine.uuid = vaccine.uuid;
-        vaccination.simpleAdverse_reaction = null;
-        delete vaccination.uuid;
-        return vaccination;
-    };
-
-    $scope.saveVaccination = function (enteredAdminFormData) {
-        // When saving an administered vaccination ensure no scheduled
-        // date is saved.
-        var enteredAdminFormDataCopy = angular.copy(enteredAdminFormData);
-        var vaccination = $scope.transformVaccineToVaccination(enteredAdminFormData);
-        vaccination.scheduled_date = null;
-        // This field is the vaccine_id and needs to be removed on posts to create new vaccinations.
-        vaccination.id = null;
-        vaccinationsManager.submitVaccination(vaccination);
-    };
-
-    $scope.scheduleVaccination = function (enteredAdminFormData) {
-        // When scheduling remove date properties that are setup for
-        // administration.
-        var enteredAdminFormDataCopy = angular.copy(enteredAdminFormData);
-        var vaccination = $scope.transformVaccineToVaccination(enteredAdminFormData);
-        vaccination.administration_date = null;
-        vaccination.manufacture_date = null;
-        vaccination.expiry_date = null;
-        vaccination.id = null;
-        vaccinationsManager.submitVaccination(vaccination);
-    };
-
-    $scope.resetFormDataToDefaults();
-}]);
-
+});
 'use strict';
 
 // Manages a vaccination instance on each vaccination
@@ -150,6 +88,68 @@ angular.module('vaccinations')
     };
 
     // Form data inits.
+    $scope.resetFormDataToDefaults();
+}]);
+
+'use strict';
+
+angular.module('vaccinations')
+.controller('StagedVaccinationController', ['$scope', '$filter', 'vaccinationsManager',
+    function ($scope, $filter, vaccinationsManager) {
+    $scope.state = {};
+    $scope.state.administerFormOpen = true;
+
+    $scope.removeStagedVaccination = function () {
+        vaccinationsManager.removeStagedVaccination();
+    };
+
+    $scope.resetFormDataToDefaults = function () {
+        var vaccination = angular.copy($scope.getVaccination());
+        if (vaccination.custom) {
+            vaccination.name = '';
+        }
+        vaccination.administration_date = new Date();
+        vaccination.scheduled_date = new Date();
+        $scope.enteredAdminFormData = vaccination;
+    };
+
+    // Transform a vaccine object into a vaccination object that
+    // can be submitted to the vaccinations endpoint.
+    $scope.transformVaccineToVaccination = function (vaccine) {
+        var vaccination = angular.copy(vaccine);
+        // Create nested objects
+        debugger;
+        vaccination.adverse_reaction_observed = false;
+        vaccination.simpleVaccine = {};
+        vaccination.simpleVaccine.uuid = vaccine.uuid;
+        vaccination.simpleAdverse_reaction = null;
+        delete vaccination.uuid;
+        return vaccination;
+    };
+
+    $scope.saveVaccination = function (enteredAdminFormData) {
+        // When saving an administered vaccination ensure no scheduled
+        // date is saved.
+        var enteredAdminFormDataCopy = angular.copy(enteredAdminFormData);
+        var vaccination = $scope.transformVaccineToVaccination(enteredAdminFormData);
+        vaccination.scheduled_date = null;
+        // This field is the vaccine_id and needs to be removed on posts to create new vaccinations.
+        vaccination.id = null;
+        vaccinationsManager.submitVaccination(vaccination);
+    };
+
+    $scope.scheduleVaccination = function (enteredAdminFormData) {
+        // When scheduling remove date properties that are setup for
+        // administration.
+        var enteredAdminFormDataCopy = angular.copy(enteredAdminFormData);
+        var vaccination = $scope.transformVaccineToVaccination(enteredAdminFormData);
+        vaccination.administration_date = null;
+        vaccination.manufacture_date = null;
+        vaccination.expiry_date = null;
+        vaccination.id = null;
+        vaccinationsManager.submitVaccination(vaccination);
+    };
+
     $scope.resetFormDataToDefaults();
 }]);
 
@@ -749,7 +749,7 @@ angular.module('vaccinations')
 
 // Constants for this instance of the app
 angular.module('vaccinations')
-.service('appConstants', ["$http", "$location", function ($http, $location) {
+.service('appConstants', function ($http, $location) {
     var tempURL;
     if (document.getElementsByTagName("title")[0].innerHTML !== "KMRI") {
         tempURL = 'http://208.77.196.178:64000';
@@ -779,7 +779,7 @@ angular.module('vaccinations')
     };
 
     return exports;
-}]);
+});
 'use strict';
 
 angular.module('vaccinations')
