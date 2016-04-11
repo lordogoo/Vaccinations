d48af52b2b205a3468167177dc2b5e5b9f3b4a36
lordogoo <mnicoll@ualberta.ca>
2016-04-11 15:39:23 -0700

    limited dates
    limited dates to be four digits as per kmri 707. Removed extraneous
    library call.

--- omod/src/main/java/org/openmrs/module/vaccinations/web/controller/VaccinationsModuleManageController.java
+++ omod/src/main/java/org/openmrs/module/vaccinations/web/controller/VaccinationsModuleManageController.java
@@ -131,5 +131,5 @@ public class  VaccinationsModuleManageController {
     public int daysBetween(Date d1,Date d2){
         return (int)((d2.getTime()-d1.getTime())/(1000*60*60*24));
     }
-    
+
 }
